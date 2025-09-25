cfdce2058bebf968dd4a55ce163c06e5e0a6f12f
Tikhon Karpukhin <fonkeep@mail.ru>
2025-09-25 15:42:48 +0300

    0.0.6

--- blogicum/blogicum/views.py
+++ blogicum/blogicum/views.py
@@ -6,6 +6,7 @@ from django.http import Http404
 def csrf_failure(request, reason=''):
     return render(request, 'pages/403csrf.html', status=403)
 
+
 def page_not_found(request, exception):
     return render(request, 'pages/404.html', status=404)
 
